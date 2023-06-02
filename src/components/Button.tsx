278ef2b5e74715a53615bc75413d9db1bdc51ed1
Pedro Fialho <2073533+pedrofialho@users.noreply.github.com>
2023-06-02 19:30:45 -0300

    Add cva

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,36 @@
 import { forwardRef, type ButtonHTMLAttributes } from 'react'
+import { cva, type VariantProps } from 'cva'
 import { cn } from '@/lib/utils'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant: 'primary' | 'secondary'
+  variant?: VariantProps<typeof buttonVariants>
 }
 
+const buttonVariants = cva('rounded', {
+  variants: {
+    intent: {
+      primary: 'border-transparent bg-blue-500 text-white hover:bg-blue-600',
+      secondary: 'border-gray-400 bg-white text-gray-800 hover:bg-gray-100',
+    },
+    size: {
+      small: 'px-2 py-1 text-sm',
+      medium: 'px-4 py-2 text-base',
+    },
+  },
+  defaultVariants: {
+    intent: 'primary',
+    size: 'medium',
+  },
+})
+
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ variant = 'secondary', className, ...props }, ref) => (
+  ({ variant, className, ...props }, ref) => (
     <button
       ref={ref}
-      className={cn(
-        className,
-        {
-          primary: 'bg-green-600 text-white',
-          secondary: 'bg-slate-50 text-slate-900',
-        }[variant],
-      )}
+      className={cn(buttonVariants(variant), className)}
       {...props}
     />
   ),
 )
+
 Button.displayName = 'Button'
