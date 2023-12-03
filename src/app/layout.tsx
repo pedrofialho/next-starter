189b3575e4e03e3488ee3dbc160394feb2a7310c
Pedro Fialho <2073533+pedrofialho@users.noreply.github.com>
2023-12-02 22:06:06 -0300

    Add Providers component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+
+import Providers from './providers'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
