83c57b62f6239713467aee7cf0e253c5c80c89f6
Pedro Fialho <2073533+pedrofialho@users.noreply.github.com>
2024-01-12 22:33:45 -0300

    Remove trailing comma in Prettier config
    Removing as the default is `'all'`

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  trailingComma: 'all',
   singleQuote: true,
   semi: false,
   plugins: [require.resolve('prettier-plugin-tailwindcss')],
