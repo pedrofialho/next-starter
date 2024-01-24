1157a26efc18033255a1a3b0a7338cb31a4c65bf
Pedro Fialho <2073533+pedrofialho@users.noreply.github.com>
2024-01-24 17:46:36 -0300

    Bump next to 14.1.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
