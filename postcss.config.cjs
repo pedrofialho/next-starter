4a4fbee203d5551080244ee3c9b1ea327cb0b6e9
Pedro Fialho <2073533+pedrofialho@users.noreply.github.com>
2024-07-21 14:28:02 -0300

    Fix PostCSS config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,9 @@
 /** @type {import("postcss-load-config").Config} */
-module.exports = {
+const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
+
+module.exports = config
