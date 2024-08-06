e45f729859b7941500dee907831ab5b9127d169f
Pedro Fialho <2073533+pedrofialho@users.noreply.github.com>
2024-08-06 00:07:20 -0300

    Add theme switcher

--- src/app/client-providers.tsx
+++ src/app/client-providers.tsx
@@ -3,7 +3,17 @@
 import { type PropsWithChildren } from 'react'
 
 import { ReactQueryProvider } from './react-query-provider'
+import { ThemeProvider } from './theme-provider'
 
 export const ClientProviders = ({ children }: PropsWithChildren) => (
-  <ReactQueryProvider>{children}</ReactQueryProvider>
+  <ReactQueryProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  </ReactQueryProvider>
 )
